a5bd154ba7cbff5aaadc82e013204f3628555548
River Phillips <riverphillips1@gmail.com>
2018-05-15 16:30:04 +0100

    Created Hourly CRON Job to calculate the rating for every beer
    Signed-off-by: River Phillips <riverphillips1@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,60 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+admin.initializeApp(functions.config().firebase);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const countries: string[] = [
+  "Austria",
+  "Belgium",
+  "Bulgaria",
+  "Croatia",
+  "Cyprus",
+  "Czech_Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Ireland",
+  "Italy",
+  "Latvia",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Netherlands",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Slovakia",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "United_Kingdom"
+];
+
+const db = admin.database();
+const ref = db.ref("/");
+//Once an hour reduce the rating multiplier so new beer's appear higher
+exports.hourly_job = functions.pubsub.topic("hourly-tick").onPublish(event => {
+  countries.forEach(country => {
+    const itemRef = ref.child("countries/" + country + "/beers");
+    itemRef.on(
+      "child_added",
+      function(snapshot) {
+        let rating: number = snapshot.val().rating;
+        if (rating > 1) {
+          rating--;
+        }
+        const beer = snapshot.val();
+        beer.rating = rating;
+        return snapshot.ref.child("rating").set(rating);
+      },
+      err => {
+        console.log("Error");
+        return "Error";
+      }
+    );
+  });
 });
